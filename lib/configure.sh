97d68481a5c5c555d466811873887b927ebc87c4
Tobias Mock <mail@tmock.de>
2022-07-27 21:29:21 +0200

    Don't print to stderr in llvm configure script

--- lib/configure.sh
+++ lib/configure.sh
@@ -8,7 +8,7 @@ fi
 
 shopt -s nullglob
 for llvm_config in llvm-config-$version llvm-config-${version}.0 llvm-config${version}0 llvm-config-mp-$version llvm-config-mp-${version}.0 llvm${version}-config llvm-config-${version}-32 llvm-config-${version}-64 llvm-config $brew_llvm_config; do
-    llvm_version="`$llvm_config --version`" || true
+    llvm_version="`$llvm_config --version 2> /dev/null`" || true
     case $llvm_version in
     $version*)
         echo $($llvm_config --cxxflags)
