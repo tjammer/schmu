23c300572ce0fe943e4053583a5c083575dee8a9
Tobias Mock <mail@tmock.de>
2022-09-30 22:41:26 +0200

    Simplify module object loading
    we don't need to keep track of the prelude flag here, as the prelude is
    automatically picked up by the Modules module if prelude is used

--- lib/link.ml
+++ lib/link.ml
@@ -2,19 +2,15 @@ let prelude_obj () =
   (* If we made it to here, the prelude is found and available *)
   Filename.remove_extension (Option.get !Module.prelude_path) ^ ".o"
 
-let link ~prelude outname objects cargs =
+let link outname objects cargs =
   (* Invoke 'cc' with all the files here *)
-  let modules =
+  let objects =
     Hashtbl.fold
       (fun name _ l ->
-        match name with
-        | "prelude" -> l
-        | name ->
-            let f = Module.find_file name ".o" in
-            f :: l)
+        let f = Module.find_file name ".o" in
+        f :: l)
       Module.module_cache objects
   in
-  let objects = if prelude then prelude_obj () :: modules else modules in
   let cmd =
     Printf.sprintf "cc -o %s %s.o %s" outname outname
       (String.concat " " (objects @ cargs))
