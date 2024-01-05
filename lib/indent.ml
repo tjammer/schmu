64263c0a015e8ed9b7fae437debc3030fe16994e
Tobias Mock <mail@tmock.de>
2024-01-05 20:03:45 +0100

    Allow closing of blocks with paren of outer block

--- lib/indent.ml
+++ lib/indent.ml
@@ -1,6 +1,6 @@
 exception Error of string
 
-type state =
+type state_kind =
   | Default
   | Marked of int
     (* We have reached a special token and a block might start. Stores line number *)
@@ -9,124 +9,139 @@ type state =
   | Newline
 (* We emit [End]s until the indentation matches again. Stores column number *)
 
-let state = ref Default
-let first = ref true
-let indents = ref [ 0 ]
-let cached_token = ref None
-let mark pos = state := Marked Lexing.(pos.pos_lnum)
+type state = {
+  kind : state_kind;
+  first : bool;
+  indents : int list;
+  cached : Parser.token option;
+  parens : int list;
+      (* Saving the indentation of opening parens let's us close indented blocks
+         with the outer paren. This is useful in higher-order functions. Otherwise,
+         the closing paren would have to go to another line to the original column.
+         The parser takes care that the correct paren is used, we don't have to
+         distinguish them. Thus, we only save the column number. *)
+}
 
-let reset () =
-  state := Default;
-  first := true;
-  indents := [ 0 ];
-  cached_token := None
+let default =
+  { kind = Default; first = true; indents = [ 0 ]; cached = None; parens = [] }
+
+let saved_state = ref default
+let mark state pos = { state with kind = Marked Lexing.(pos.pos_lnum) }
+let reset () = saved_state := default
 
 let get_cnum lexbuf =
   let pos = Lexing.(lexbuf.lex_start_p) in
   pos.pos_cnum - pos.pos_bol
 
-let rec emit lexbuf (token : Parser.token) =
+let rec emit state lexbuf (token : Parser.token) =
   match token with
   | Colon ->
-      state := Marked Lexing.(lexbuf.lex_curr_p.pos_lnum);
-      token
+      let state = mark state Lexing.(lexbuf.lex_curr_p) in
+      (token, state)
   | Eof -> (
       (* Insert dedents before Eof if there still is indentation *)
-      match !indents with
+      match state.indents with
       | [] -> raise (Error "Inconsistent indentation")
-      | [ _ ] -> token
+      | [ _ ] -> (token, state)
       | _ :: _ ->
-          cached_token := Some token;
-          state := Dedent 0;
-          dedent lexbuf 0)
-  | token -> token
+          dedent { state with kind = Dedent 0; cached = Some token } lexbuf 0)
+  | Lpar | Lbrac | Lbrack | Hashtag_brack | Hashnum_brack _ ->
+      (token, { state with parens = List.hd state.indents :: state.parens })
+  | Rbrac | Rbrack | Rpar -> (
+      match (state.parens, state.indents) with
+      | parcol :: tl, cnum :: _ ->
+          (* Leave the parcol in state until the indent is correct *)
+          if parcol < cnum then
+            dedent
+              { state with kind = Dedent parcol; cached = Some token }
+              lexbuf parcol
+          else (token, { state with parens = tl })
+      | [], _ -> (token, state)
+      | _, _ -> failwith "Internal Error in parens thing")
+  | token -> (token, state)
 
-and maybe_newline lexbuf =
-  match !cached_token with
+and maybe_newline state lexbuf =
+  match state.cached with
   | Some
       (( Parser.Rbrac | Rbrack | Rpar | Right_arrow | Pipe_tail | Else | Elseif
        | Eof ) as token) ->
       (* These tokes should be able to be placed on the outer indent without
          starting a new line. *)
-      state := Default;
-      emit lexbuf token
-  | _ ->
-      state := Newline;
-      Newline
+      emit { state with kind = Default } lexbuf token
+  | _ -> (Newline, { state with kind = Newline })
 
-and dedent lexbuf cnum =
+and dedent state lexbuf cnum =
   (* We emit [End] until we found the matching indentation *)
-  match !indents with
+  match state.indents with
   | [] -> raise (Error "Inconsintent indentation")
   | column :: tl ->
-      if cnum < column then (
-        (* Still dedenting *)
-        indents := tl;
-        Parser.(End))
+      if cnum < column then
+        ((* Still dedenting *)
+         Parser.(End), { state with indents = tl })
       else if cnum = column then
         (* We have reached the correct indent *)
-        maybe_newline lexbuf
+        maybe_newline state lexbuf
       else
         (* We have missed our column  *)
         raise (Error "Inconsintent indentation")
 
-let indent lexbuf =
+let indent state lexbuf =
   (* The indentation happens in [read_marked], we just emit the cached token *)
-  state := Default;
-  match !cached_token with
-  | Some token -> emit lexbuf token
+  match state.cached with
+  | Some token -> emit { state with kind = Default } lexbuf token
   | None -> failwith "Internal indent error"
 
 let newline = indent
 
-let read_default lexbuf =
+let read_default state lexbuf =
   let token = Lexer.read lexbuf in
 
-  let indent_cnum = List.hd !indents in
+  let indent_cnum = List.hd state.indents in
   let cnum = get_cnum lexbuf in
-  if cnum < indent_cnum then (
+  (* let state = *)
+  (*   match token with *)
+  (*   | Lpar | Lbrac | Lbrack | Hashtag_brack | Hashnum_brack _ -> *)
+  (*       Printf.printf "incr parens to %i\n%!" cnum; *)
+  (*       { state with parens = cnum :: state.parens } *)
+  (*   | _ -> state *)
+  (* in *)
+  if cnum < indent_cnum then
     (* We are dedenting *)
-    cached_token := Some token;
-    state := Dedent cnum;
-    dedent lexbuf cnum)
-  else if cnum = indent_cnum && not !first then (
-    cached_token := Some token;
-    maybe_newline lexbuf)
-  else (
-    first := false;
-    emit lexbuf token)
+    dedent { state with cached = Some token; kind = Dedent cnum } lexbuf cnum
+  else if cnum = indent_cnum && not state.first then
+    maybe_newline { state with cached = Some token } lexbuf
+  else emit { state with first = false } lexbuf token
 
-let read_marked lexbuf lnum =
+let read_marked state lexbuf lnum =
   let next_token = Lexer.read lexbuf in
   let pos = lexbuf.lex_start_p in
   let cnum = pos.pos_cnum - pos.pos_bol in
 
-  let indent_cnum = List.hd !indents in
+  let indent_cnum = List.hd state.indents in
   (* If we are on the same line, there is no indent *)
-  if lnum = pos.pos_lnum then (
-    state := Default;
-    next_token)
-  else if cnum > indent_cnum then (
-    indents := cnum :: !indents;
-    state := Indent;
-    (* Save the token to emit on next call and insert [Begin] *)
-    cached_token := Some next_token;
-    Parser.(Begin))
+  if lnum = pos.pos_lnum then
+    emit { state with kind = Default } lexbuf next_token
+  else if cnum > indent_cnum then
+    ( (* Save the token to emit on next call and insert [Begin] *)
+      Parser.(Begin),
+      {
+        state with
+        indents = cnum :: state.indents;
+        kind = Indent;
+        cached = Some next_token;
+      } )
   else
     (* We are on a new line after [Equal], but not indented *)
     raise (Error "Expected an indented line")
 
-let string_of_state = function
-  | Default -> "default"
-  | Marked lnum -> Printf.sprintf "marked %i" lnum
-  | Indent -> "indent"
-  | Dedent cnum -> Printf.sprintf "dedent %i" cnum
-  | Newline -> "newline"
-
 let insert_ends lexbuf =
-  match !state with
-  | Default -> read_default lexbuf
-  | Marked lnum -> read_marked lexbuf lnum
-  | Indent -> indent lexbuf
-  | Dedent cnum -> dedent lexbuf cnum
-  | Newline -> newline lexbuf
+  let token, state =
+    match !saved_state.kind with
+    | Default -> read_default !saved_state lexbuf
+    | Marked lnum -> read_marked !saved_state lexbuf lnum
+    | Indent -> indent !saved_state lexbuf
+    | Dedent cnum -> dedent !saved_state lexbuf cnum
+    | Newline -> newline !saved_state lexbuf
+  in
+  saved_state := state;
+  token
