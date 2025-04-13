32e422ccf778e93203a8bbe978d6061ec04a1e2c
Tobias Mock <mail@tmock.de>
2025-04-13 23:08:44 +0200

    Correct impl for second test
    Don't pass because error message doesn't match

--- lib/typing/borrows.ml
+++ lib/typing/borrows.ml
@@ -1,7 +1,9 @@
 module Borrow = struct
-  type action = Read | Write
-  type action_location = Foreign | Local
+  type action = Read | Write [@@deriving show]
+  type action_location = Foreign | Local [@@deriving show]
+
   type state = Reserved | Unique | Frozen | Disabled | Reserved_im | Owned
+  [@@deriving show]
 
   let transition curr action =
     match (curr, action) with
@@ -16,8 +18,8 @@ module Borrow = struct
     | Frozen, (Write, Foreign) -> Ok Disabled
     | Reserved_im, (Read, (Local | Foreign) | Write, Foreign) -> Ok Reserved_im
     | Reserved_im, (Write, Local) -> Ok Unique
-    | Disabled, (Read, (Local | Foreign)) -> Ok Disabled
-    | Disabled, (Write, (Local | Foreign)) -> Error ()
+    | Disabled, ((Read | Write), Foreign) -> Ok Disabled
+    | Disabled, ((Read | Write), Local) -> Error ()
     | Owned, _ -> Ok Owned
 end
 
@@ -32,6 +34,8 @@ end
 module type Id_t = sig
   type t
 
+  val pp : Format.formatter -> t -> unit
+  val show : t -> string
   val equal : t -> t -> bool
   val compare : t -> t -> int
 
@@ -42,9 +46,9 @@ module type Id_t = sig
 end
 
 module Make_tree (Id : Id_t) = struct
-  type id = Id.t
-  type t = { state : state; id : id; children : t list }
-  type path = id list
+  type id = Id.t [@@deriving show]
+  type t = { state : state; id : id; children : t list } [@@deriving show]
+  type path = id list [@@deriving show]
   type access = { ac : action; path : path }
 
   let transition_exn state acc location =
@@ -56,6 +60,7 @@ module Make_tree (Id : Id_t) = struct
     let rec aux found path item =
       match path with
       | p :: ptl when Id.equal item.id p ->
+          print_endline (Id.show item.id ^ " local");
           let state = transition_exn item.state access.ac Local in
           let found = match ptl with [] -> true | _ -> found in
           let found, children =
@@ -65,6 +70,7 @@ module Make_tree (Id : Id_t) = struct
           in
           (found, { item with state; children })
       | _ ->
+          print_endline (Id.show item.id ^ " foreign");
           let state = transition_exn item.state access.ac Foreign in
           let found, children =
             List.fold_left_map
@@ -131,7 +137,8 @@ module Make_storage (Id : Id_t) = struct
         let found, tree =
           Tree.bind id { ac; path } (Index_map.find i st.trees)
         in
-        (found, { st with trees = Index_map.add i tree st.trees })
+        let indices = Id_map.add id (path @ [ id ], i) st.indices in
+        (found, { trees = Index_map.add i tree st.trees; indices })
     | None -> (false, st)
 
   let insert id state st =
@@ -140,6 +147,27 @@ module Make_storage (Id : Id_t) = struct
     let trees = Index_map.add i Tree.{ state; id; children = [] } st.trees in
     let indices = Id_map.add id ([ id ], i) st.indices in
     { trees; indices }
+
+  let print st =
+    print_endline "******************";
+    String.concat ", "
+      (Id_map.to_seq st.indices
+      |> Seq.map (fun (id, (path, i)) ->
+             Id.show id ^ ": (" ^ Tree.show_path path ^ ", " ^ string_of_int i
+             ^ ")")
+      |> List.of_seq)
+    |> print_endline;
+    let spaces i = String.make i ' ' in
+    let rec show_tree sp t =
+      spaces sp ^ Id.show Tree.(t.id) ^ "= " ^ show_state t.state
+      |> print_endline;
+      List.iter (fun t -> show_tree (sp + 2) t) t.children
+    in
+    Index_map.iter
+      (fun i t ->
+        print_endline ("t " ^ string_of_int i);
+        show_tree 2 t)
+      st.trees
 end
 
 module Make_ids (Id : Id_t) = struct
@@ -176,28 +204,51 @@ type state = { trees : Trst.t; ids : Idst.t; mname : Path.t }
 
 let state_empty mname = { trees = Trst.empty; ids = Idst.empty; mname }
 
-let rec check_expr st tyex =
+let rec check_expr st ac tyex =
   (* Pass trees back up the typed tree, because we need to maintain its state.
      Ids on the other hand follow lexical scope *)
+  Trst.print st.trees;
   match tyex.expr with
   | Const _ -> (None, st.trees)
   | Var (str, mname) ->
+      print_endline ("var " ^ str ^ ", " ^ show_action ac);
       let id = Idst.get str mname st.ids in
-      let found, trees = Trst.borrow id Read st.trees in
+      let found, trees = Trst.borrow id ac st.trees in
       if found then (Some id, trees) else (None, trees)
   | App { callee; args } ->
-      let _, trees = check_expr st callee in
+      print_endline "call";
+      let _, trees = check_expr st Read callee in
       let trees =
         List.fold_left
-          (fun trees (arg, _attr) ->
-            let _, trees = check_expr { st with trees } arg in
+          (fun trees (arg, attr) ->
+            let ac =
+              match attr with
+              | Dmut | Dset -> Write
+              | Dnorm -> Read
+              | Dmove -> failwith "not yet move"
+            in
+            let _, trees = check_expr { st with trees } ac arg in
             trees)
           trees args
       in
       (None, trees)
-  | _ -> (None, st.trees)
+  | Set (expr, value, _moved) ->
+      print_endline "set";
+      let _, trees = check_expr st Read value in
+      let _, trees = check_expr { st with trees } Write expr in
+      (None, trees)
+  | Let { id; lmut; pass; rhs; cont; _ } ->
+      print_endline "let";
+      let st = check_let st ~toplevel:false id pass lmut rhs in
+      check_expr st ac cont
+  | Sequence (fst, snd) ->
+      let _, trees = check_expr st Read fst in
+      check_expr { st with trees } ac snd
+  | _ ->
+      (* print_endline ("none: " ^ show_typed_expr tyex); *)
+      (None, st.trees)
 
-let check_let st ~toplevel str pass lmut rhs =
+and check_let st ~toplevel str pass lmut rhs =
   print_endline (string_of_bool toplevel);
   if toplevel && pass = Dmut then
     raise (Error (rhs.loc, "Cannot project at top level"))
@@ -206,7 +257,8 @@ let check_let st ~toplevel str pass lmut rhs =
   else
     let bid, ids = Idst.insert str (Some st.mname) st.ids in
     let trees =
-      match check_expr st rhs with
+      (* TODO [Read] is not always true here *)
+      match check_expr st Read rhs with
       | None, trees ->
           (* Nothing is borrowed, we own this *)
           Trst.insert bid Owned trees
@@ -223,13 +275,13 @@ let check_item st = function
   | Tl_let { id; pass; rhs; lmut; _ } ->
       check_let ~toplevel:true st id pass lmut rhs
   | Tl_expr e ->
-      let _, trees = check_expr st e in
+      let _, trees = check_expr st Read e in
       { st with trees }
   | Tl_function _ -> st
   | Tl_bind (str, e) ->
       let bid, ids = Idst.insert str (Some st.mname) st.ids in
       let trees =
-        match check_expr st e with
+        match check_expr st Read e with
         | None, trees -> Trst.insert bid Frozen trees
         | Some rhs_id, trees ->
             let found, trees = Trst.bind bid Read rhs_id trees in
@@ -240,7 +292,8 @@ let check_item st = function
   | _ -> failwith "TODO"
 
 let check_expr ~mname expr =
-  check_expr (state_empty mname) expr |> ignore
+  (* [Read] should be move later *)
+  check_expr (state_empty mname) Read expr |> fun (_, trees) -> Trst.print trees
 
 let check_items ~mname items =
   ignore Write;
