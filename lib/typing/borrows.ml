79742831b0956bd7e9263cd19362195f831eddf2
Tobias Mock <mail@tmock.de>
2025-04-14 22:42:52 +0200

    Add locations to state

--- lib/typing/borrows.ml
+++ lib/typing/borrows.ml
@@ -5,22 +5,28 @@ module Borrow = struct
   type state = Reserved | Unique | Frozen | Disabled | Reserved_im | Owned
   [@@deriving show]
 
-  let transition curr action =
+  let transition curr action action_loc =
+    (* Keep location next to state to remember the location of whatever brought
+       us into the current state. If a transition is invalid, we can point to
+       whatever brought us here. If the state doesn't change, keep the first
+       location.*)
+    let curr, loc = curr in
     match (curr, action) with
-    | Reserved, (Read, (Foreign | Local)) -> Ok Reserved
-    | Reserved, (Write, Local) -> Ok Unique
-    | Reserved, (Write, Foreign) -> Ok Disabled
-    | Unique, ((Read | Write), Local) -> Ok Unique
-    | Unique, (Read, Foreign) -> Ok Frozen
-    | Unique, (Write, Foreign) -> Ok Disabled
-    | Frozen, (Read, (Local | Foreign)) -> Ok Frozen
-    | Frozen, (Write, Local) -> Error ()
-    | Frozen, (Write, Foreign) -> Ok Disabled
-    | Reserved_im, (Read, (Local | Foreign) | Write, Foreign) -> Ok Reserved_im
-    | Reserved_im, (Write, Local) -> Ok Unique
-    | Disabled, ((Read | Write), Foreign) -> Ok Disabled
-    | Disabled, ((Read | Write), Local) -> Error ()
-    | Owned, _ -> Ok Owned
+    | Reserved, (Read, (Foreign | Local)) -> Ok (Reserved, loc)
+    | Reserved, (Write, Local) -> Ok (Unique, action_loc)
+    | Reserved, (Write, Foreign) -> Ok (Disabled, action_loc)
+    | Unique, ((Read | Write), Local) -> Ok (Unique, loc)
+    | Unique, (Read, Foreign) -> Ok (Frozen, action_loc)
+    | Unique, (Write, Foreign) -> Ok (Disabled, action_loc)
+    | Frozen, (Read, (Local | Foreign)) -> Ok (Frozen, loc)
+    | Frozen, (Write, Local) -> Error `None
+    | Frozen, (Write, Foreign) -> Ok (Disabled, action_loc)
+    | Reserved_im, (Read, (Local | Foreign) | Write, Foreign) ->
+        Ok (Reserved_im, loc)
+    | Reserved_im, (Write, Local) -> Ok (Unique, action_loc)
+    | Disabled, ((Read | Write), Foreign) -> Ok (Disabled, loc)
+    | Disabled, ((Read | Write), Local) -> Error (`Disabled (loc, action_loc))
+    | Owned, _ -> Ok (Owned, loc)
 end
 
 open Borrow
@@ -38,6 +44,7 @@ module type Id_t = sig
   val show : t -> string
   val equal : t -> t -> bool
   val compare : t -> t -> int
+  val only_id : t -> string
 
   module Pathid : Pathid
 
@@ -47,21 +54,36 @@ end
 
 module Make_tree (Id : Id_t) = struct
   type id = Id.t [@@deriving show]
-  type t = { state : state; id : id; children : t list } [@@deriving show]
+  type loc_info = { lid : Id.t; loc : Ast.loc } [@@deriving show]
+
+  type t = {
+    state : state * loc_info;
+    id : id;
+    bind_loc : loc_info;
+    children : t list;
+  }
+  [@@deriving show]
+
   type path = id list [@@deriving show]
   type access = { ac : action; path : path }
 
-  let transition_exn state acc location =
-    match transition state (acc, location) with
+  let transition_exn item acc location loc =
+    match transition item.state (acc, location) loc with
     | Ok state -> state
-    | Error () -> failwith "err"
+    | Error (`Disabled ({ lid; loc = _ }, action)) ->
+        let msg =
+          Format.sprintf "%s was borrowed in line %i, cannot mutate"
+            (Id.only_id lid) (fst item.bind_loc.loc).pos_lnum
+        in
+        raise (Error.Error (action.loc, msg))
+    | Error `None -> failwith "not yet none"
 
-  let borrow access tree =
+  let borrow access loc tree =
     let rec aux found path item =
       match path with
       | p :: ptl when Id.equal item.id p ->
           print_endline (Id.show item.id ^ " local");
-          let state = transition_exn item.state access.ac Local in
+          let state = transition_exn item access.ac Local loc in
           let found = match ptl with [] -> true | _ -> found in
           let found, children =
             List.fold_left_map
@@ -71,7 +93,7 @@ module Make_tree (Id : Id_t) = struct
           (found, { item with state; children })
       | _ ->
           print_endline (Id.show item.id ^ " foreign");
-          let state = transition_exn item.state access.ac Foreign in
+          let state = transition_exn item access.ac Foreign loc in
           let found, children =
             List.fold_left_map
               (fun found tree -> aux found path tree)
@@ -81,7 +103,7 @@ module Make_tree (Id : Id_t) = struct
     in
     aux false access.path tree
 
-  let bind id access tree =
+  let bind id bind_loc access tree =
     (* Similar to borrow, but we don't transition and instead add a node if we
        find the access node *)
     let rec aux found path item =
@@ -91,9 +113,11 @@ module Make_tree (Id : Id_t) = struct
             match ptl with
             | [] ->
                 let state =
-                  match access.ac with Read -> Frozen | Write -> Reserved
+                  match access.ac with
+                  | Read -> (Frozen, bind_loc)
+                  | Write -> (Reserved, bind_loc)
                 in
-                (true, { state; id; children = [] } :: item.children)
+                (true, { state; id; bind_loc; children = [] } :: item.children)
             | _ ->
                 List.fold_left_map
                   (fun found tree -> aux found ptl tree)
@@ -122,29 +146,36 @@ module Make_storage (Id : Id_t) = struct
     incr id;
     !id
 
-  let borrow id ac st =
-    match Id_map.find_opt id st.indices with
+  let borrow lid loc ac st =
+    match Id_map.find_opt lid st.indices with
     | Some (path, i) ->
         let found, tree =
-          Tree.borrow { ac; path } (Index_map.find i st.trees)
+          Tree.borrow { ac; path } { lid; loc } (Index_map.find i st.trees)
         in
         (found, { st with trees = Index_map.add i tree st.trees })
     | None -> (false, st)
 
-  let bind id ac bound st =
+  let bind id loc ac bound st =
     match Id_map.find_opt bound st.indices with
     | Some (path, i) ->
+        let loc = { Tree.lid = bound; loc } in
         let found, tree =
-          Tree.bind id { ac; path } (Index_map.find i st.trees)
+          Tree.bind id loc { ac; path } (Index_map.find i st.trees)
         in
         let indices = Id_map.add id (path @ [ id ], i) st.indices in
         (found, { trees = Index_map.add i tree st.trees; indices })
     | None -> (false, st)
 
-  let insert id state st =
+  let insert id bind_loc state st =
     assert (Id_map.mem id st.indices |> not);
     let i = fresh () in
-    let trees = Index_map.add i Tree.{ state; id; children = [] } st.trees in
+    let loc_info = { Tree.lid = id; loc = bind_loc } in
+    let state = (state, loc_info) in
+    let trees =
+      Index_map.add i
+        Tree.{ state; id; bind_loc = loc_info; children = [] }
+        st.trees
+    in
     let indices = Id_map.add id ([ id ], i) st.indices in
     { trees; indices }
 
@@ -159,7 +190,7 @@ module Make_storage (Id : Id_t) = struct
     |> print_endline;
     let spaces i = String.make i ' ' in
     let rec show_tree sp t =
-      spaces sp ^ Id.show Tree.(t.id) ^ "= " ^ show_state t.state
+      spaces sp ^ Id.show Tree.(t.id) ^ "= " ^ show_state (fst t.state)
       |> print_endline;
       List.iter (fun t -> show_tree (sp + 2) t) t.children
     in
@@ -213,7 +244,7 @@ let rec check_expr st ac tyex =
   | Var (str, mname) ->
       print_endline ("var " ^ str ^ ", " ^ show_action ac);
       let id = Idst.get str mname st.ids in
-      let found, trees = Trst.borrow id ac st.trees in
+      let found, trees = Trst.borrow id tyex.loc ac st.trees in
       if found then (Some id, trees) else (None, trees)
   | App { callee; args } ->
       print_endline "call";
@@ -237,9 +268,9 @@ let rec check_expr st ac tyex =
       let _, trees = check_expr st Read value in
       let _, trees = check_expr { st with trees } Write expr in
       (None, trees)
-  | Let { id; lmut; pass; rhs; cont; _ } ->
+  | Let { id; lmut; pass; rhs; cont; id_loc; _ } ->
       print_endline "let";
-      let st = check_let st ~toplevel:false id pass lmut rhs in
+      let st = check_let st ~toplevel:false id id_loc pass lmut rhs in
       check_expr st ac cont
   | Sequence (fst, snd) ->
       let _, trees = check_expr st Read fst in
@@ -248,7 +279,7 @@ let rec check_expr st ac tyex =
       (* print_endline ("none: " ^ show_typed_expr tyex); *)
       (None, st.trees)
 
-and check_let st ~toplevel str pass lmut rhs =
+and check_let st ~toplevel str loc pass lmut rhs =
   print_endline (string_of_bool toplevel);
   if toplevel && pass = Dmut then
     raise (Error (rhs.loc, "Cannot project at top level"))
@@ -261,10 +292,10 @@ and check_let st ~toplevel str pass lmut rhs =
       match check_expr st Read rhs with
       | None, trees ->
           (* Nothing is borrowed, we own this *)
-          Trst.insert bid Owned trees
+          Trst.insert bid loc Owned trees
       | Some rhs_id, trees ->
           (* Even if we borrow mutably, we don't write here yet *)
-          let found, trees = Trst.bind bid Read rhs_id trees in
+          let found, trees = Trst.bind bid loc Read rhs_id trees in
           assert found;
           trees
     in
@@ -272,8 +303,8 @@ and check_let st ~toplevel str pass lmut rhs =
     { st with ids; trees }
 
 let check_item st = function
-  | Tl_let { id; pass; rhs; lmut; _ } ->
-      check_let ~toplevel:true st id pass lmut rhs
+  | Tl_let { id; pass; rhs; lmut; loc; _ } ->
+      check_let ~toplevel:true st id loc pass lmut rhs
   | Tl_expr e ->
       let _, trees = check_expr st Read e in
       { st with trees }
@@ -282,9 +313,9 @@ let check_item st = function
       let bid, ids = Idst.insert str (Some st.mname) st.ids in
       let trees =
         match check_expr st Read e with
-        | None, trees -> Trst.insert bid Frozen trees
+        | None, trees -> Trst.insert bid e.loc Frozen trees
         | Some rhs_id, trees ->
-            let found, trees = Trst.bind bid Read rhs_id trees in
+            let found, trees = Trst.bind bid e.loc Read rhs_id trees in
             assert found;
             trees
       in
