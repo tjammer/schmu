ab2196daf3df19d4673af88f2f11276a85c75f62
Tobias Mock <mail@tmock.de>
2026-01-17 23:01:43 +0100

    Respect `CC` environment variable when linking

--- lib/link.ml
+++ lib/link.ml
@@ -1,8 +1,13 @@
 let link outname objects cargs =
   (* Invoke 'cc' with all the files here *)
   let objects = objects @ Module.object_names () in
+  let cc =
+    match Sys.getenv_opt "CC" with
+    | None -> "cc"
+    | Some path -> if String.length path <> 0 then path else "cc"
+  in
   let cmd =
-    Printf.sprintf "cc -o %s %s %s.o %s" outname
+    Printf.sprintf "%s -o %s %s %s.o %s" cc outname
       (String.concat " " objects)
       outname (String.concat " " cargs)
   in
